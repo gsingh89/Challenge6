5a8e51fe95b6245f48db593e22cbd4dbbae06b9d
Ubuntu <vagrant@Base-Box>
2017-11-13 22:39:44 +0000

    Part 3

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Customer do
 
-permit_params :full_name, :phone_number, :email_address, :notes, :image
+permit_params :full_name, :phone_number, :email_address, :notes, :image, :province_id
 
 end
