d112cb4fa9544170f616fd35cdc72e278b21385a
Ubuntu <vagrant@Base-Box>
2017-11-13 22:13:48 +0000

    Part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113202544) do
+ActiveRecord::Schema.define(version: 20171113215819) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171113202544) do
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
